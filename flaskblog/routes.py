f618c43f5c06fe9f52c9f6ef3fb41dfe1b9bbf0f
ushernj <ushernjuguna@gmail.com>
2025-12-02 16:32:56 +0300

    Name changes. add

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -10,13 +10,13 @@ from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Usher Njuguna',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Magdalene Nyambura',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
